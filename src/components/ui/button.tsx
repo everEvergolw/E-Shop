311cda47bc75a5f164820cf73b8923c2990361de
everEvergolw <1092547508@qq.com>
2024-08-22 02:03:50 +0100

    design:creating configuration summary page

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -60,9 +60,13 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
+        
         ref={ref}
+
         {...props}>
+
         {isLoading && loadingText ? loadingText : children}
+
         {isLoading ? (
           <span className='ml-1.5 flex items-center gap-1'>
             <span className='animate-flashing w-1 h-1 bg-white rounded-full inline-block' />
