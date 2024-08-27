6416346f293afaef04b3c8790f05162dde18a736
everEvergolw <1092547508@qq.com>
2024-08-27 01:56:44 +0100

    add metadata

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -19,7 +19,7 @@ export function constructMetadata({
   title = 'CaseCobra - custom high-quality phone cases',
   description = 'Create custom high-quality phone cases in seconds',
   image = '/thumbnail.png',
-  icons = '/favicon.ico',
+  icons = '/favicon.ico', 
 }: {
   title?: string
   description?: string
@@ -39,9 +39,9 @@ export function constructMetadata({
       title,
       description,
       images: [image],
-      creator: '@joshtriedcoding',
+      creator: '@jiahaoliu', 
     },
     icons,
-    metadataBase: new URL("https://casecobra.vercel.app/")
+    metadataBase: new URL("https://casecobra-dev-taupe.vercel.app/")
   }
 }
